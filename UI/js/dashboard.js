6c60652a3bb14eab3ae3351af356309ded5cbbab
themastermind <bertrandmasabo22@gmail.com>
2019-02-15 09:58:29 +0200

    Improved admin dashboard

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
   $("#parties").css("color", "#555");
   // Edit party
   $(".fa-edit").on("click", function(){
-    $(".data").css("visibility", "hidden");
-  	$(".data").css("display", "none");
-    $(".edit-data").css("visibility", "visible");
-  	$(".edit-data").css("display", "table-cell");
+    $(this).closest(".data").css("visibility", "hidden");
+  	$(this).closest(".data").css("display", "none");
+    $(this).closest(".edit-data").css("visibility", "visible");
+  	$(this).closest(".edit-data").css("display", "table-cell");
   });
   // Abort edit
   $(".abort").on("click", function(){
@@ -15,5 +15,19 @@ $(document).ready(function(){
   	$(".edit-data").css("display", "none");
     $(".data").css("visibility", "visible");
   	$(".data").css("display", "table-cell");
-  });	        
+  });
+  // Navigate side menu
+  $('#parties').on('click', function() {
+    $('.office-data').css('display', 'none');
+    $('.petition-data').css('display', 'none');
+    $('.user-data').css('display', 'none');
+    $('.party-data').css('display', 'block');
+    $('').css('display', 'block');
+  });
+  $('#offices').on('click', function() {
+    $('.party-data').css('display', 'none');
+    $('.petition-data').css('display', 'none');
+    $('.user-data').css('display', 'none');
+    $('.office-data').css('display', 'block');
+  });
 });
